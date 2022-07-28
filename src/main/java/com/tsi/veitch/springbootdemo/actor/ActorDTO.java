ff3f23d4d19c91c5ffaa681b55b1c8f5db4f729e
Veitchie <104382261+Veitchie@users.noreply.github.com>
2022-07-28 14:39:25 +0100

    adding new actors and updating names

---
+++ src/main/java/com/tsi/veitch/springbootdemo/actor/ActorDTO.java
@@ -0,0 +1,2 @@
+package com.tsi.veitch.springbootdemo.actor;public class ActorDTO {
+}
--- src/main/java/com/tsi/veitch/springbootdemo/film_actor/FilmActorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tsi.veitch.springbootdemo.film_actor;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FilmActorRepository extends CrudRepository<FilmActor,Integer> {
-}
