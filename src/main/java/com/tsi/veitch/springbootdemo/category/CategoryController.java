69f14750a80be9cc20f5ade80c922ca460b4fdcd
Veitchie <104382261+Veitchie@users.noreply.github.com>
2022-07-28 16:30:19 +0100

    category controller hotfix

--- src/main/java/com/tsi/veitch/springbootdemo/category/CategoryController.java
+++ src/main/java/com/tsi/veitch/springbootdemo/category/CategoryController.java
@@ -14,7 +14,7 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public CategoryController(CategoryRepository filmRepository){
+    public CategoryController(CategoryRepository categoryRepository){
         this.categoryRepository = categoryRepository;
     }
 
