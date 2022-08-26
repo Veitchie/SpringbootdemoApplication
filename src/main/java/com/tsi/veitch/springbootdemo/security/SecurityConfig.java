007f07e28db623ab5d72557c13a76232fda4403b
Veitchie <104382261+Veitchie@users.noreply.github.com>
2022-08-26 09:16:16 +0100

    Auth0

--- src/main/java/com/tsi/veitch/springbootdemo/security/SecurityConfig.java
+++ src/main/java/com/tsi/veitch/springbootdemo/security/SecurityConfig.java
@@ -1,52 +1,56 @@
 package com.tsi.veitch.springbootdemo.security;
 
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
-public class SecurityConfig {
+import java.util.Arrays;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .mvcMatchers("/Sakila/film").permitAll()
-                .mvcMatchers("/Sakila/actor").permitAll()
-                .mvcMatchers("/Sakila/actor/newActor").authenticated()
-                .mvcMatchers("/Sakila/actor/removeActor").authenticated()
-                .mvcMatchers("/Sakila/actor/updateName").authenticated()
-                .and().cors()
-                .and().oauth2ResourceServer().jwt();
-        http.csrf().disable();
-        return http.build();
-    }
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${auth0.audience}")
-    private String audience;
-
+    private String apiAudience;
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuer;
 
     @Bean
-    JwtDecoder jwtDecoder() {
-        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
-                JwtDecoders.fromOidcIssuerLocation(issuer);
-
-        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-
-        jwtDecoder.setJwtValidator(withAudience);
-
-        return jwtDecoder;
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH","DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        JwtWebSecurityConfigurer
+                .forRS256(apiAudience, issuer)
+                .configure(http)
+                .authorizeRequests()
+
+                // Note: If passing an Authorization header, Spring Security will validate it even with permitAll()
+                // You can ignore security filters if this is an issue for you, as discussed here:
+                // https://stackoverflow.com/questions/36296869/spring-security-permitall-still-considering-token-passed-in-authorization-header
+                .antMatchers(HttpMethod.GET, "/Sakila/film").permitAll()
+                .antMatchers(HttpMethod.GET, "/Sakila/actor").permitAll()
+                //.antMatchers(HttpMethod.GET, "/Sakila/private").authenticated()
+                .antMatchers(HttpMethod.GET, "/Sakila/actor/newActor").hasAuthority("write:actors");
+    }
+
 }
